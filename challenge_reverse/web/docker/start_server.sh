9b9e9f984fbab1763f5f4be935d49a6bba2ad99c
Jean-Baptiste Decourcelle <jeanbaptiste.decourcelle@gmail.com>
2021-02-19 19:48:33 +0100

    docker ok

---
+++ challenge_reverse/web/docker/start_server.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo docker run -d --name shopping_express_site -p 6789:80 shopping_express
+
---
+++ challenge_reverse/web/docker/stop_server.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo docker stop shopping_express_site
+sudo docker rm shopping_express_site
+
