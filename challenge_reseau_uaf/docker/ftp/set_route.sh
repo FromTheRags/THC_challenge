dfaf88d3a3fb69dbb7678b7f23836c686f20c471
jb <jb@xubuntu.fr>
2021-05-09 01:23:49 +0200

    added attack container + exploit script

--- challenge_reseau_uaf/docker/ftp/set_route.sh
+++ challenge_reseau_uaf/docker/ftp/set_route.sh
@@ -2,4 +2,3 @@
 
 ip route del default
 ip route add default via 10.42.2.2
-
--- challenge_reseau_uaf/docker/reset_docker.sh
+++ challenge_reseau_uaf/docker/reset_docker.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-sudo ip route del 10.42.2.0/24
-sudo ip route del 10.42.3.0/24
 sudo docker kill $(sudo docker ps --quiet)
 sudo docker system prune --all --force
 
